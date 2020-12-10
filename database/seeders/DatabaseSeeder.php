b20923ddbd53e74cdb2327c749bf33c29df9ac47
sunny <sunny.king1219@gmail.com>
2020-12-11 00:33:42 +0100

    fdfdfd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder {
     */
 
     public function run() {
-        $this->call( [UsersTableSeeder::class] );
+        // $this->call( [UsersTableSeeder::class] );
         // $this->call( [CategoryTableSeeder::class] );
         // $this->call( [BreedTableSeeder::class] );
         // $this->call( [AppSettingTableSeeder::class] );
