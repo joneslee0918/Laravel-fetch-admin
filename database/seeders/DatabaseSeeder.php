0934522a0ff2c51400617450c97b29607a6aee3c
sunny <sunny.king1219@gmail.com>
2020-12-11 00:31:12 +0100

    dsdsdsdsds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,18 +4,17 @@ namespace Database\Seeders;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $this->call([UsersTableSeeder::class]);
-        $this->call([CategoryTableSeeder::class]);
-        $this->call([BreedTableSeeder::class]);
-        $this->call([AppSettingTableSeeder::class]);
+    * Seed the application's database.
+    *
+    * @return void
+    */
+
+    public function run() {
+        $this->call( [UsersTableSeeder::class] );
+        // $this->call( [CategoryTableSeeder::class] );
+        // $this->call( [BreedTableSeeder::class] );
+        // $this->call( [AppSettingTableSeeder::class] );
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,22 +5,37 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
-class UsersTableSeeder extends Seeder
-{
+class UsersTableSeeder extends Seeder {
     /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('users')->insert([
-            'name' => 'Admin Admin',
-            'email' => 'admin@admin.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('secret'),
+    * Run the database seeds.
+    *
+    * @return void
+    */
+
+    public function run() {
+        // DB::table( 'users' )->insert( [
+        //     'name' => 'Admin Admin',
+        //     'email' => 'admin@admin.com',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make( 'secret' ),
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ] );
+
+        DB::table( 'user_meta' )->insert( [
+            'id_user' => '1',
+            'meta_key' => '_show_notification',
+            'meta_value' => 1,
             'created_at' => now(),
             'updated_at' => now()
-        ]);
+        ] );
+
+        DB::table( 'user_meta' )->insert( [
+            'id_user' => '1',
+            'meta_key' => '_show_phone_on_ads',
+            'meta_value' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ] );
     }
 }
