f7d13b5f7e1cb8de59406abd0b7a72d78b659b8d
sunny <sunny.king1219@gmail.com>
2020-12-11 19:48:42 +0100

    privacy

---
+++ public/privacy.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <title>Privacy Policy – Zimbo Charades</title>
+    <link rel="dns-prefetch" href="http://fonts.googleapis.com/">
+    <link rel="dns-prefetch" href="http://s.w.org/">
+    <link rel="alternate" type="application/rss+xml" title="HOURGLASS GAMES » Kanał z wpisami"
+        href="http://www.hourglassgames.pl/index.php/feed/">
+    <link rel="alternate" type="application/rss+xml" title="HOURGLASS GAMES » Kanał z komentarzami"
+        href="http://www.hourglassgames.pl/index.php/comments/feed/">
+
+    <link rel="stylesheet" id="wp-block-library-css" href="./material\privacy_css/style.min.css" type="text/css"
+        media="all">
+    <link rel="stylesheet" id="cookie-notice-front-css" href="./material\privacy_css/front.min.css" type="text/css"
+        media="all">
+    <link rel="stylesheet" id="onepress-fonts-css" href="./material\privacy_css/css" type="text/css" media="all">
+    <link rel="stylesheet" id="onepress-animate-css" href="./material\privacy_css/animate.min.css" type="text/css"
+        media="all">
+    <link rel="stylesheet" id="onepress-fa-css" href="./material\privacy_css/font-awesome.min.css" type="text/css"
+        media="all">
+    <link rel="stylesheet" id="onepress-bootstrap-css" href="./material\privacy_css/bootstrap.min.css" type="text/css"
+        media="all">
+    <link rel="stylesheet" id="onepress-style-css" href="./material\privacy_css/style.css" type="text/css" media="all">
+    <link rel="stylesheet" id="onepress-gallery-lightgallery-css" href="./material\privacy_css/lightgallery.css"
+        type="text/css" media="all">
+    <link rel="stylesheet" id="elementor-icons-css" href="./material\privacy_css/elementor-icons.min.css"
+        type="text/css" media="all">
+    <link rel="stylesheet" id="elementor-animations-css" href="./material\privacy_css/animations.min.css"
+        type="text/css" media="all">
+    <link rel="stylesheet" id="elementor-frontend-css" href="./material\privacy_css/frontend.min.css" type="text/css"
+        media="all">
+    <link rel="stylesheet" id="elementor-global-css" href="./material\privacy_css/global.css" type="text/css"
+        media="all">
+    <link rel="stylesheet" id="google-fonts-1-css" href="./material\privacy_css/css(1)" type="text/css" media="all">
+    <script type="text/javascript">
+    /* <![CDATA[ */
+    var cnArgs = {
+        "ajaxUrl": "http:\/\/www.hourglassgames.pl\/wp-admin\/admin-ajax.php",
+        "nonce": "b957075830",
+        "hideEffect": "fade",
+        "position": "bottom",
+        "onScroll": "0",
+        "onScrollOffset": "100",
+        "onClick": "0",
+        "cookieName": "cookie_notice_accepted",
+        "cookieTime": "2592000",
+        "cookieTimeRejected": "2592000",
+        "cookiePath": "\/",
+        "cookieDomain": "",
+        "redirection": "0",
+        "cache": "0",
+        "refuse": "0",
+        "revokeCookies": "0",
+        "revokeCookiesOpt": "automatic",
+        "secure": "0",
+        "coronabarActive": "0"
+    };
+    /* ]]> */
+    </script>
+    <script type="text/javascript">
+    /* <![CDATA[ */
+    var onepress_js_settings = {
+        "onepress_disable_animation": "",
+        "onepress_disable_sticky_header": "",
+        "onepress_vertical_align_menu": "",
+        "hero_animation": "flipInX",
+        "hero_speed": "5000",
+        "hero_fade": "750",
+        "hero_duration": "5000",
+        "hero_disable_preload": "",
+        "is_home": "",
+        "gallery_enable": "",
+        "is_rtl": ""
+    };
+    /* ]]> */
+    </script>
+    <script type="text/javascript" src="./material\privacy_css/jquery.js.download"></script>
+    <script type="text/javascript" src="./material\privacy_css/jquery-migrate.min.js.download"></script>
+
+<body
+    class="page-template-default page page-id-1495 wp-custom-logo elementor-default elementor-kit-1345 elementor-page elementor-page-1495 body-desktop cookies-set cookies-accepted"
+    data-elementor-device-mode="desktop">
+    <div id="page" class="hfeed site">
+        <a class="skip-link screen-reader-text"
+            href="http://www.hourglassgames.pl/index.php/charades-word-generator-app-privacy-policy/#content">Przeskocz
+            do treści</a>
+        <div id="header-section" class="h-on-top no-transparent">
+            <div class="followWrap" style="height: 75px;">
+                <header id="masthead"
+                    class="site-header header-contained is-sticky no-scroll no-t h-on-top header-fixed" role="banner"
+                    style="top: 0px;">
+                    <div class="container">
+                        <div class="site-branding">
+                            <div class="site-brand-inner has-logo-img no-desc">
+                                <div class="site-logo-div"><a href="#" class="custom-logo-link  no-t-logo" rel="home"
+                                        itemprop="url"><img width="64" height="64" src="./material\privacy_css/icon.png"
+                                            class="custom-logo" alt="ZIMBO CHARADES GAMES" itemprop="logo"></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            </div><!-- #masthead -->
+        </div>
+        <div class="page-header">
+            <div class="container">
+                <h1 class="entry-title">Privacy Policy – Fetch App</h1>
+            </div>
+        </div>
+
+	<div style="height:1000px">
+	</div>
+
+        <footer id="colophon" class="site-footer" role="contentinfo">
+
+            <div class="site-info">
+                <div class="container">
+                    <div class="btt">
+                        <a class="back-to-top" href="#page">
+                        </a>
+                    </div>
+                    Darryl © 2020 FETCH APP <span class="sep"></span>
+                </div>
+            </div>
+            <!-- .site-info -->
+
+        </footer><!-- #colophon -->
+    </div><!-- #page -->
+
+
+    <script type="text/javascript" src="./material\privacy_css/plugins.js.download"></script>
+    <script type="text/javascript" src="./material\privacy_css/bootstrap.min.js.download"></script>
+    <script type="text/javascript" src="./material\privacy_css/theme.js.download"></script>
+    <script type="text/javascript" src="./material\privacy_css/wp-embed.min.js.download"></script>
+    <script type="text/javascript" src="./material\privacy_css/frontend-modules.min.js.download"></script>
+    <script type="text/javascript" src="./material\privacy_css/position.min.js.download"></script>
+    <script type="text/javascript" src="./material\privacy_css/dialog.min.js.download"></script>
+    <script type="text/javascript" src="./material\privacy_css/waypoints.min.js.download"></script>
+    <script type="text/javascript" src="./material\privacy_css/swiper.min.js.download"></script>
+    <script type="text/javascript" src="./material\privacy_css/share-link.min.js.download"></script>
+    <script type="text/javascript">
+    var elementorFrontendConfig = {
+        "environmentMode": {
+            "edit": false,
+            "wpPreview": false
+        },
+        "i18n": {
+            "shareOnFacebook": "Share on Facebook",
+            "shareOnTwitter": "Share on Twitter",
+            "pinIt": "Pin it",
+            "downloadImage": "Download image"
+        },
+        "is_rtl": false,
+        "breakpoints": {
+            "xs": 0,
+            "sm": 480,
+            "md": 768,
+            "lg": 1025,
+            "xl": 1440,
+            "xxl": 1600
+        },
+        "version": "2.9.7",
+        "urls": {
+            "assets": "http:\/\/www.hourglassgames.pl\/wp-content\/plugins\/elementor\/assets\/"
+        },
+        "settings": {
+            "page": [],
+            "general": {
+                "elementor_global_image_lightbox": "yes",
+                "elementor_lightbox_enable_counter": "yes",
+                "elementor_lightbox_enable_fullscreen": "yes",
+                "elementor_lightbox_enable_zoom": "yes",
+                "elementor_lightbox_enable_share": "yes",
+                "elementor_lightbox_title_src": "title",
+                "elementor_lightbox_description_src": "description"
+            },
+            "editorPreferences": []
+        },
+        "post": {
+            "id": 1495,
+            "excerpt": "",
+            "featuredImage": false
+        }
+    };
+    </script>
+    <script type="text/javascript" src="./material\privacy_css/frontend.min.js.download"></script><span
+        id="elementor-device-mode" class="elementor-screen-only"></span>
+
+</body>
+
+</html>
