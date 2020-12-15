12a69822ec5d5500c48efc6bac8a9caa9574bc17
sunny <sunny.king1219@gmail.com>
2020-12-15 23:04:29 +0100

    fdfdfdfdfdfd

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:api',
+            'throttle:600,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
