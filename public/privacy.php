f9cd3a3b982ad7d9703ed38d5d73a748c3b154c3
sunny <sunny.king1219@gmail.com>
2020-12-15 02:30:41 +0100

    fdfdfdf

--- public/privacy.php
+++ public/privacy.php
@@ -38,45 +38,45 @@
         media="all">
     <link rel="stylesheet" id="google-fonts-1-css" href="./material\privacy_css/css(1)" type="text/css" media="all">
     <script type="text/javascript">
-    /* <![CDATA[ */
-    var cnArgs = {
-        "ajaxUrl": "http:\/\/www.hourglassgames.pl\/wp-admin\/admin-ajax.php",
-        "nonce": "b957075830",
-        "hideEffect": "fade",
-        "position": "bottom",
-        "onScroll": "0",
-        "onScrollOffset": "100",
-        "onClick": "0",
-        "cookieName": "cookie_notice_accepted",
-        "cookieTime": "2592000",
-        "cookieTimeRejected": "2592000",
-        "cookiePath": "\/",
-        "cookieDomain": "",
-        "redirection": "0",
-        "cache": "0",
-        "refuse": "0",
-        "revokeCookies": "0",
-        "revokeCookiesOpt": "automatic",
-        "secure": "0",
-        "coronabarActive": "0"
-    };
+        /* <![CDATA[ */
+        var cnArgs = {
+            "ajaxUrl": "http:\/\/www.hourglassgames.pl\/wp-admin\/admin-ajax.php",
+            "nonce": "b957075830",
+            "hideEffect": "fade",
+            "position": "bottom",
+            "onScroll": "0",
+            "onScrollOffset": "100",
+            "onClick": "0",
+            "cookieName": "cookie_notice_accepted",
+            "cookieTime": "2592000",
+            "cookieTimeRejected": "2592000",
+            "cookiePath": "\/",
+            "cookieDomain": "",
+            "redirection": "0",
+            "cache": "0",
+            "refuse": "0",
+            "revokeCookies": "0",
+            "revokeCookiesOpt": "automatic",
+            "secure": "0",
+            "coronabarActive": "0"
+        };
     /* ]]> */
     </script>
     <script type="text/javascript">
-    /* <![CDATA[ */
-    var onepress_js_settings = {
-        "onepress_disable_animation": "",
-        "onepress_disable_sticky_header": "",
-        "onepress_vertical_align_menu": "",
-        "hero_animation": "flipInX",
-        "hero_speed": "5000",
-        "hero_fade": "750",
-        "hero_duration": "5000",
-        "hero_disable_preload": "",
-        "is_home": "",
-        "gallery_enable": "",
-        "is_rtl": ""
-    };
+        /* <![CDATA[ */
+        var onepress_js_settings = {
+            "onepress_disable_animation": "",
+            "onepress_disable_sticky_header": "",
+            "onepress_vertical_align_menu": "",
+            "hero_animation": "flipInX",
+            "hero_speed": "5000",
+            "hero_fade": "750",
+            "hero_duration": "5000",
+            "hero_disable_preload": "",
+            "is_home": "",
+            "gallery_enable": "",
+            "is_rtl": ""
+        };
     /* ]]> */
     </script>
     <script type="text/javascript" src="./material\privacy_css/jquery.js.download"></script>
@@ -85,7 +85,7 @@
 <body
     class="page-template-default page page-id-1495 wp-custom-logo elementor-default elementor-kit-1345 elementor-page elementor-page-1495 body-desktop cookies-set cookies-accepted"
     data-elementor-device-mode="desktop">
-    <div id="page" class="hfeed site">
+    <div id="page" class="hfeed site" style="overflow-x: hidden;">
         <a class="skip-link screen-reader-text"
             href="http://www.hourglassgames.pl/index.php/charades-word-generator-app-privacy-policy/#content">Przeskocz
             do treści</a>
@@ -112,8 +112,84 @@
             </div>
         </div>
 
-	<div style="height:1000px">
-	</div>
+        <div class="row" style="margin-top: 20px;">
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8">
+                Our Privacy Policy is designed to assist you in understanding how we collect and use the personal
+                information you provide to us and to assist you in making informed decisions when using our site and our
+                products and services.
+
+                <br>
+                <h4 style="margin-top:20px">What Information Do We Collect?</h4>
+
+                When you visit our Web site you may provide us with two types of information: personal information you
+                knowingly choose to disclose that is collected on an individual basis and Web site use information
+                collected on an aggregate basis as you and others browse our Web site.
+
+                <br>
+                <h4 style="margin-top:20px">Personal Information You Choose to Provide</h4>
+
+
+                <br>
+                <h6>Service Request Information.</h6>
+
+                You will provide us information about yourself, your pets, and the services you are looking for when you
+                fill out a service request form on fetchpetcare.com, or register for email newsletters and alerts. You
+                may also provide additional comments on how you see fetchpetcare.com servicing your needs and interests.
+                Any information you add to your listing will be stored. Fetch! Pet Care and its associated franchisees
+                will have access to this information
+
+                <h4 style="margin-top:20px;">Fetch Applications.</h4>
+
+                You will provide us information about yourself, and the services you are capable of performing for
+                Fetch! Pet Care and its associated franchises when you fill out a Fetch application form on
+                fetchpetcare.com, or register for email newsletters and alerts. You may also provide additional comments
+                on how you and Fetch! Pet Care can work together. Any information you add to your application will be
+                stored.
+
+                <h4 style="margin-top: 20px;">Email Information.</h4>
+
+                If you choose to correspond with us through email, we retain the content of your email messages together
+                with your email address and our responses.
+
+                Web Site Use Information.
+
+                Similar to other commercial Web sites, our Web site utilizes sessions to track user information and Web
+                server logs/analytics to collect information about how our Web site is used. Information gathered
+                through sessions and Web server logs may include the date and time of visits, the pages viewed, time
+                spent at our Web site, and the Web sites visited just before and just after our Web site. We, our
+                advertisers and ad serving companies may also use small technology or pieces of code to determine which
+                advertisements and promotions users have seen and how users responded to them.
+
+
+                <h4 style="margin-top: 20px;">How Do We Use the Information That You Provide to Us?</h4>
+
+                Broadly speaking, we use personal information for purposes of administering and expanding our business
+                activities, providing customer service and making available other products and services to our customers
+                and prospective customers. Occasionally, we may also use the information we collect to notify you about
+                important changes to our Web site, new services and special offers we think you will find valuable. You
+                may notify us at any time if you do not wish to receive these offers by contacting us.
+
+
+                <h4 style="margin-top: 20px;">Notice of New Services and Changes</h4>
+
+                Occasionally, we may also use the information we collect to notify you about important changes to our
+                Web site, new services and special offers we think you will find valuable. As our customer, you will be
+                given the opportunity to notify us of your desire not to receive these offers by sending us a request
+                via our contact us page.
+
+                How Do We Secure Information Transmissions?
+
+                Email is not recognized as a secure medium of communication. For this reason, we request that you do not
+                send private information to us by email. Some of the information you may enter on our Web site may be
+                transmitted securely via Secure Sockets Layer SSL, 128 bit encryption services. Pages utilizing this
+                technology will have URLs that start with HTTPS instead of HTTP. Please contact us if you have any
+                questions or concerns. Payments will be made via a third party processor and payment information should
+                not be submitted via the contact us page..
+            </div>
+            <div class="col-sm-2"></div>
+
+        </div>
 
         <footer id="colophon" class="site-footer" role="contentinfo">
 
@@ -123,7 +199,8 @@
                         <a class="back-to-top" href="#page">
                         </a>
                     </div>
-                    Darryl © 2020 FETCH APP <span class="sep"><a style="margin-left:100px">darryl@buybitcoins.site<a/></span>
+                    Darryl © 2020 FETCH APP <span class="sep"><a
+                            style="margin-left:100px">darryl@buybitcoins.site<a /></span>
                 </div>
             </div>
             <!-- .site-info -->
@@ -143,49 +220,49 @@
     <script type="text/javascript" src="./material\privacy_css/swiper.min.js.download"></script>
     <script type="text/javascript" src="./material\privacy_css/share-link.min.js.download"></script>
     <script type="text/javascript">
-    var elementorFrontendConfig = {
-        "environmentMode": {
-            "edit": false,
-            "wpPreview": false
-        },
-        "i18n": {
-            "shareOnFacebook": "Share on Facebook",
-            "shareOnTwitter": "Share on Twitter",
-            "pinIt": "Pin it",
-            "downloadImage": "Download image"
-        },
-        "is_rtl": false,
-        "breakpoints": {
-            "xs": 0,
-            "sm": 480,
-            "md": 768,
-            "lg": 1025,
-            "xl": 1440,
-            "xxl": 1600
-        },
-        "version": "2.9.7",
-        "urls": {
-            "assets": "http:\/\/www.hourglassgames.pl\/wp-content\/plugins\/elementor\/assets\/"
-        },
-        "settings": {
-            "page": [],
-            "general": {
-                "elementor_global_image_lightbox": "yes",
-                "elementor_lightbox_enable_counter": "yes",
-                "elementor_lightbox_enable_fullscreen": "yes",
-                "elementor_lightbox_enable_zoom": "yes",
-                "elementor_lightbox_enable_share": "yes",
-                "elementor_lightbox_title_src": "title",
-                "elementor_lightbox_description_src": "description"
+        var elementorFrontendConfig = {
+            "environmentMode": {
+                "edit": false,
+                "wpPreview": false
             },
-            "editorPreferences": []
-        },
-        "post": {
-            "id": 1495,
-            "excerpt": "",
-            "featuredImage": false
-        }
-    };
+            "i18n": {
+                "shareOnFacebook": "Share on Facebook",
+                "shareOnTwitter": "Share on Twitter",
+                "pinIt": "Pin it",
+                "downloadImage": "Download image"
+            },
+            "is_rtl": false,
+            "breakpoints": {
+                "xs": 0,
+                "sm": 480,
+                "md": 768,
+                "lg": 1025,
+                "xl": 1440,
+                "xxl": 1600
+            },
+            "version": "2.9.7",
+            "urls": {
+                "assets": "http:\/\/www.hourglassgames.pl\/wp-content\/plugins\/elementor\/assets\/"
+            },
+            "settings": {
+                "page": [],
+                "general": {
+                    "elementor_global_image_lightbox": "yes",
+                    "elementor_lightbox_enable_counter": "yes",
+                    "elementor_lightbox_enable_fullscreen": "yes",
+                    "elementor_lightbox_enable_zoom": "yes",
+                    "elementor_lightbox_enable_share": "yes",
+                    "elementor_lightbox_title_src": "title",
+                    "elementor_lightbox_description_src": "description"
+                },
+                "editorPreferences": []
+            },
+            "post": {
+                "id": 1495,
+                "excerpt": "",
+                "featuredImage": false
+            }
+        };
     </script>
     <script type="text/javascript" src="./material\privacy_css/frontend.min.js.download"></script><span
         id="elementor-device-mode" class="elementor-screen-only"></span>
