adcb449626ae853b2e4e27e59ad27c012c4ea4e1
sunny <sunny.king1219@gmail.com>
2021-01-05 20:19:13 +0100

    saas

--- app/Http/Controllers/WEB/UserController.php
+++ app/Http/Controllers/WEB/UserController.php
@@ -32,7 +32,7 @@ class UserController extends Controller {
     public function index() {
         //
         $data = array();
-        $users = User::where( 'role', '0' )->where('is_social', '!=', '-1')->get();
+        $users = User::where( 'role', '0' )->where( 'is_social', '!=', '-1' )->get();
         $data['users'] = $users;
 
         return view( 'user.index', ['data' => $data] );
@@ -188,7 +188,7 @@ class UserController extends Controller {
         $next_active = $user->active;
         if ( $prev_active != $next_active ) {
             $this->email->sendMail( $user->email, 5, $next_active == 1 ? 'activated' : 'deactivated' );
-            $this->notification->send($user->id, "account_status", "Account ".$next_active == 1 ?'Activated':'Deactivated', "Your account has been ".$next_active == 1 ?'activated':'deactivated'." by administrator.", null, $user);
+            $this->notification->send( $user->id, 'account_status', 'Account '.$next_active == 1 ?'Activated':'Deactivated', 'Your account has been '.( $next_active == 1 ?'activated':'deactivated' ).' by administrator.', null, $user );
         } else {
             $this->email->sendMail( $user->email, 4, null );
         }
