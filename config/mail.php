3bc1b1002b7fe8b66690bc7b63bc980f328924fc
sunny <sunny.king1219@gmail.com>
2021-01-06 21:20:51 +0100

    dsds

--- config/mail.php
+++ config/mail.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'driver' => env( 'MAIL_DRIVER', 'smtp' ),
+    'driver' => env( 'MAIL_DRIVER', 'sendmail' ),
 
     /*
     |--------------------------------------------------------------------------
@@ -71,7 +71,7 @@ return [
     |
     */
 
-    'encryption' => env( 'MAIL_ENCRYPTION', 'ssl' ),
+    'encryption' => env( 'MAIL_ENCRYPTION', 'tls' ),
 
     /*
     |--------------------------------------------------------------------------
